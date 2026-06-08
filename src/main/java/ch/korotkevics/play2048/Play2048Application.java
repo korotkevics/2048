691e08799012b5f83b6095821c7701a27f7d97f4
Sandor Korotkevics <korotkevics.sandor@gmail.com>
2026-06-08 22:34:16 +0200

    Init

---
+++ src/main/java/ch/korotkevics/play2048/Play2048Application.java
@@ -0,0 +1,13 @@
+package ch.korotkevics.play2048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Play2048Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Play2048Application.class, args);
+    }
+
+}
---
+++ src/test/java/ch/korotkevics/play2048/Play2048ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.korotkevics.play2048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Play2048ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
